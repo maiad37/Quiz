52305fdc6cb5feea5b576d6d9877bc20078e609a
Maia Dinsmore <69313479+maiad37@users.noreply.github.com>
2022-03-13 20:34:39 -0300

    Create item.jsx

---
+++ src/components/Item/item.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './item.css'
+const Item = (props) => {
+    return(
+        <div>
+            <p className="title">{props.title}</p>
+            <button value='true'>True</button>
+            <button value='false'>False</button>
+        </div>
+    )
+}
+
+export default Item
