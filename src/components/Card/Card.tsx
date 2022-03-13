0ad8e77e7efb1a2e2a63cb01c357e0477fce29f8
Maia Dinsmore <69313479+maiad37@users.noreply.github.com>
2022-03-13 20:34:29 -0300

    Create Card.tsx

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Quiz from "../Quiz/Quiz";
+import './Card.css'
+
+const Card = () => {
+    return(
+        <div className="container">
+            <h1>Quiz</h1>
+            <Quiz/>
+        </div>
+    )
+}
+
+export default Card;
