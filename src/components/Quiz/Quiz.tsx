8e079aba02a9acddb64866a8ee3c1a9f208bcb67
Maia Dinsmore <69313479+maiad37@users.noreply.github.com>
2022-03-20 15:46:40 -0300

    Update Quiz.tsx

--- src/components/Quiz/Quiz.tsx
+++ src/components/Quiz/Quiz.tsx
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from "react";
-import './Quiz.css'
+import "./Quiz.css";
 import axios from "axios";
 
-const Quiz:React.FC = () => {
+const Quiz: React.FC = () => {
+  const [questions, setQuestions] = useState([]),
+    [qNum, setQnum] = useState(0),
+    [userAnswer, setUserAnswer] = useState("undeff"),
+    [correct, setCorrect] = useState(""),
+    [score, setScore] = useState(0);
   // interface Q{
   //     category:string;
   //     type:"boolean";
@@ -12,51 +17,77 @@ const Quiz:React.FC = () => {
   //     question:string;
   // }
 
-   const nextQuestion = () => {
-     if( qNum <10){
-      setQnum(qNum+1)
+  const nextQuestion = () => {
+    if (qNum < 10) {
+      setQnum(qNum + 1);
     }
-   }
+  };
 
-   const checkAnswer = () => {
-     if(userAnswer===correct){
-      console.log("you are correct!")
-      var Tbtn = document.getElementById("true")
-      Tbtn?.style.backgroundColor = "green";
-     }else{
-       console.log("you are wrong!")
-     }
-   }
-  const [questions, setQuestions] = useState([]),
-   [qNum, setQnum] = useState(0),
-   [userAnswer, setUserAnswer] = useState(""),
-   [correct, setCorrect] = useState(""), 
-   [color, setColor] = useState("grey");
+  const checkAnswer = () => {
+    if (userAnswer === correct) {
+      setScore(score + 1);
+      console.log("User answer:" + userAnswer + "\nCorrect:" + correct);
+    } else {
+      console.log("User answer:" + userAnswer + "\nCorrect:" + correct);
+    }
+  };
 
   useEffect(() => {
     axios
       .get(`https://opentdb.com/api.php?amount=10&type=boolean`)
       .then((response) => {
-        setQuestions(response.data.results[qNum].question?.replaceAll('&#039;', "'").replaceAll("&quot;", '"'));
-       setCorrect(response.data.results[qNum].correct_answer)
-       
+        setQuestions(
+          response.data.results[qNum].question
+            ?.replaceAll("&#039;", "'")
+            .replaceAll("&quot;", '"')
+        );
+        setCorrect(response.data.results[qNum].correct_answer);
+        console.log(response.data.results[qNum].correct_answer);
       });
   }, [qNum]);
 
   // const questionList = questions.map((questions: any, numb:number) => {
   //   return <Item key={numb} title={questions.question.replaceAll('&#039;', "'").replaceAll("&quot;", '"')} />;
   // });
-  return(
+  return (
     <>
       <div>
-        {`${qNum + 1} - ${questions} `}
+        <div className="questions">{`${qNum + 1} - ${questions} `}</div>
         <div className="buttons">
-          <button className="true" value="True" onClick={()=>{setUserAnswer("True"); checkAnswer()}}>True</button>
-          <button className="false" value="False" onClick={()=>{setUserAnswer("False"); checkAnswer()}}>False</button>
+          <button
+            className="true"
+            value="True"
+            onClick={() => {
+              setUserAnswer("True");
+              checkAnswer();
+            }}
+          >
+            True
+          </button>
+          <button
+            className="false"
+            value="False"
+            onClick={() => {
+              setUserAnswer("False");
+              checkAnswer();
+            }}
+          >
+            False
+          </button>
         </div>
-       <div className="nextBtn"><button className="next" onClick={nextQuestion}>Next</button></div> 
+        <p>Score: {score}/10</p>
+        <div className="nextBtn">
+          <button
+            className="next"
+            onClick={() => {
+              nextQuestion();
+            }}
+          >
+            Next
+          </button>
+        </div>
       </div>
     </>
-    )
+  );
 };
-export default Quiz;  
+export default Quiz;
