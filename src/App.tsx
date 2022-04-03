9bb721860cfa2bad5abc24cedf0bdf76dd2ee0df
Maia Dinsmore <69313479+maiad37@users.noreply.github.com>
2022-04-03 18:05:14 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <div className="LandingPage">
       <div className="grid-container">
         <div className="grid-item"><img src="https://images.unsplash.com/photo-1588139094921-01da2d0bc337?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" alt="quiz app" width="200px" height="200px"/></div>
-        <div className="grid-item"><img src="https://images.unsplash.com/photo-1588139094921-01da2d0bc337?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" alt="quiz app" width="200px"/></div>
+        <div className="grid-item"><img src="https://images.unsplash.com/photo-1588139094921-01da2d0bc337?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" alt="quiz app" width="200px" height="200px"/></div>
         <div className="grid-item"><img src="https://images.unsplash.com/photo-1588139094921-01da2d0bc337?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" alt="quiz app" width="200px" height="200px"/></div>
         <div className="grid-item"><img src="https://images.unsplash.com/photo-1588139094921-01da2d0bc337?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" alt="quiz app" width="200px" height="200px"/></div>
         
