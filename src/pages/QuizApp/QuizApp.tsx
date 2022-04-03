d46d45a8712a22224787d4e2fe8b3a472feaf236
Maia Dinsmore <69313479+maiad37@users.noreply.github.com>
2022-04-03 17:44:57 -0300

    Create QuizApp.tsx

---
+++ src/pages/QuizApp/QuizApp.tsx
@@ -0,0 +1,12 @@
+import './QuizApp.css'
+import Card from '../../components/Card/Card';
+
+function QuizApp() {
+  return (
+    <div className="QuizApp">
+      <Card/>
+    </div>
+  );
+}
+
+export default QuizApp;
