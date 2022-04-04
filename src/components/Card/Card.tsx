9d111cbae9d9863147b2ae1a87b2b10994f478cb
Maia Dinsmore <69313479+maiad37@users.noreply.github.com>
2022-04-04 18:41:54 -0300

    Update Card.tsx

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,13 +1,13 @@
 import Quiz from "../Quiz/Quiz";
-import './Card.css'
+import "./Card.css";
 
 const Card = () => {
-    return(
-        <div className="container">
-            <h1>Quiz</h1>
-            <Quiz/>
-        </div>
-    )
-}
+  return (
+    <div className="container">
+      <h1>Quiz</h1>
+      <Quiz />
+    </div>
+  );
+};
 
 export default Card;
